c7a1f4a16733f1ad742283cce4dbfdd9d7baae2c
Ben Brewer <ben.brewer@codethink.co.uk>
2015-10-12 17:44:13 +0100

    Test col 132 cutoff in freeform

---
+++ freeform132.f90
@@ -0,0 +1,3 @@
+      PROGRAM freeform132
+        INTEGER*4 number;                                                                                                           012456789
+      END PROGRAM freeform132
