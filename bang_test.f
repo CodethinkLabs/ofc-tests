56125d6d9a4faa67de8509a82f3097cda0c4556a
Ben Brewer <ben.brewer@codethink.co.uk>
2015-10-12 16:58:01 +0100

    Test bang comments

---
+++ bang_test.f
@@ -0,0 +1,3 @@
+      PROGRAM bang_test
+        INTEGER*4 number!this is a number
+      END PROGRAM bang_test
---
+++ fixed_bang.f
@@ -0,0 +1,5 @@
+      PROGRAM fixed_bang
+        CHARACTER*10 msg =
+     !    "Hello World!\n" ! This line is a continuation
+        PRINT *, msg
+      END PROGRAM fixed_bang
