53430bbc5489e353e5073efe953a3bf98ac57fa1
Ben Brewer <ben.brewer@codethink.co.uk>
2015-12-10 15:56:28 +0000

    Attach Apache 2.0 license to our tests

--- ANSI_1097.f
+++ ANSI_1097.f
@@ -1,3 +1,17 @@
+!     Copyright 2015 Codethink Ltd.
+!
+!     Licensed under the Apache License, Version 2.0 (the "License");
+!     you may not use this file except in compliance with the License.
+!     You may obtain a copy of the License at
+!
+!         http://www.apache.org/licenses/LICENSE-2.0
+!
+!     Unless required by applicable law or agreed to in writing, software
+!     distributed under the License is distributed on an "AS IS" BASIS,
+!     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+!     See the License for the specific language governing permissions and
+!     limitations under the License.
+
       program ANSI_1097
       INTEGER*4 fkey(2),lkey(2)
       INTEGER*2 i2buffer(24),i2fkey(4),i2lkey(4),dbwork(128)
