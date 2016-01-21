f56f1839eb2c5df603f20bc46a9e87332256b9ae
Craig Griffiths <craig.griffiths@codethink.co.uk>
2016-01-21 11:12:46 +0000

    Add DO WHILE with labeled END DO test and fix up prints.
    NOTE: Printing a single space to get blank lines in the output until OFC handles NULL strings.

--- do_block.f
+++ do_block.f
@@ -29,7 +29,7 @@
       PRINT *, "Result:"
       PRINT *, "I = ", I
       PRINT *, "J = ", J
-      PRINT *,""
+      PRINT *, " "
 !     **********************************************************
 !     END OF TEST 1
 !     **********************************************************
@@ -37,13 +37,13 @@
 !     TEST 2: DO LOOP
 !     **********************************************************
       PRINT *, "TEST 2: DO LOOP"
-      PRINT *, "Expected: Print all even numbers from 2-20 inclusive."
+      PRINT *, "Expected: Print all even numbers from 1-20 inclusive."
       PRINT *, "Result:"
       DO 1 J=1,10
         K=J*2
         PRINT *, K
     1 CONTINUE
-      PRINT *,""
+      PRINT *, " "
 !     **********************************************************
 !     END OF TEST 2
 !     **********************************************************
@@ -51,7 +51,7 @@
 !     TEST 3: DO LOOP WITH GOTO
 !     **********************************************************
       PRINT *, "TEST 3: DO LOOP WITH GOTO"
-      PRINT *, "Expected: Print all even numbers from 2-20 inclusive."
+      PRINT *, "Expected: Print all even numbers from 1-20 inclusive."
       PRINT *, "Result:"
       DO 2 J = 1, 10
         GO TO 3
@@ -61,8 +61,26 @@
     3 K = J * 2
       GO TO 4
     5 CONTINUE
-      PRINT *,""
+      PRINT *, " "
 !     **********************************************************
 !     END OF TEST 3
+!     **********************************************************
+!     **********************************************************
+!     TEST 4: DO WHILE WITH LABELED END DO
+!     **********************************************************
+      PRINT *, "TEST 4: DO WHILE WITH LABELED END DO"
+      PRINT *, "Expected: Print all odd numbers from 1-20 inclusive."
+      PRINT *, "Result:"
+      I = 0
+      DO WHILE(I .LT .20)
+        I = I + 1
+        IF (MOD(I, 2) .EQ. 0) THEN
+          GO TO 6
+        END IF
+        PRINT *, I
+    6 END DO
+      PRINT *, " "
+!     **********************************************************
+!     END OF TEST 4
 !     **********************************************************
       END PROGRAM DOTEST
