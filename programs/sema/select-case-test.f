f1a7d59ec48738384b537002c857ae1441b7a984
Violeta Menendez Gonzalez <violeta.menendez@codethink.co.uk>
2016-07-29 13:39:17 +0100

    Add SELECT CASE positive and negative tests

---
+++ programs/sema/select-case-test.f
@@ -0,0 +1,14 @@
+      PROGRAM sel
+        RINDEX = 2.5
+        SELECT CASE (RINDEX)
+        CASE (2:6)
+          X = 1.0
+        CASE (:1)
+          X = 2.0
+        CASE (7)
+          X = 3.0
+        CASE DEFAULT
+          X = 99.0
+        END SELECT
+        PRINT *, X
+      END PROGRAM sel
